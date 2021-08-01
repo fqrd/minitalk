0468eaa8b26baadee10f2354511e63226f834208
fcaquard <fcaquard@student.s19.be>
2021-08-01 15:33:09 +0200

    adding libs

---
+++ minitalk.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fcaquard <fcaquard@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/01 15:00:53 by fcaquard          #+#    #+#             */
+/*   Updated: 2021/08/01 15:07:05 by fcaquard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <signal.h>
+/**
+ * signal
+ * sigemptyset
+ * sigaddset
+ * sigaction
+ * kill
+ */
+#include <stdlib.h>
+/**
+ * malloc
+ * free
+ */
+#include <unistd.h>
+/**
+ * write
+ * getpid
+ * pause
+ * sleep
+ * usleep
+ * exit
+ */
+
+int main(void)
+{
+	return (0);
+}
