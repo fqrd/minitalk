3fb37ef1f92879185b45695705b2e8640d0cabbd
fcaquard <fcaquard@student.s19.be>
2021-08-02 20:05:11 +0200

    changed tactic, no malloc!

--- client.c
+++ client.c
@@ -6,82 +6,38 @@
 /*   By: fcaquard <fcaquard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/01 17:16:52 by fcaquard          #+#    #+#             */
-/*   Updated: 2021/08/02 18:34:14 by fcaquard         ###   ########.fr       */
+/*   Updated: 2021/08/02 20:03:36 by fcaquard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-
-char * to_binary(int c, char *res, int bits)
+void	to_binary(int c, int bits, int pid)
 {
-	int j;
 	int b;
 
-	j = 0;
 	while (bits >= 0)
 	{
 		b = c >> bits;
 		if (b & 1)
-			res[j] = '1';
+			kill(pid, SIGUSR1);
 		else
-			res[j] = '0';
+			kill(pid, SIGUSR2);
+		usleep(5);
 		bits--;
-		j++;
 	}
-	return (res);
-}
-
-char	*char_to_binary(int c)
-{
-	char *binary;
-	
-	if (c > 255)
-		return (NULL);
-	binary = malloc(sizeof(char) * 8 + 1);
-	if (!binary)
-		return (NULL);
-	binary[8] = '\0';
-	to_binary(c, binary, 7);
-	return (binary);
 }
 
 int		main(int argc, char *argv[])
 {
 	int		pid;
 	char	*str;
-	char	*binary;
-	int		i;
 
 	if (argc < 3)
 		return (0);
 	pid = ft_atoi(argv[1]);
 	str = argv[2];
-	binary = NULL;
-	i = 0;
-
-	printf("pid: %d\n", pid);
-	printf("str: %s\n", str);
-
 	while (*str)
-	{
-		binary = char_to_binary(*str++);
-		if (!binary)
-			return (0);
-		while (binary[i])
-		{
-			ft_putchar(binary[i]);
-			if (binary[i] == '1')
-				kill(pid, SIGUSR1);
-			else
-				kill(pid, SIGUSR2);
-			usleep(280);
-			i++;
-		}
-		ft_putchar('\n');
-		i = 0;
-		free(binary);
-		binary = NULL;
-	}
+		to_binary(*str++, 7, pid);
 	return (0);
 }
--- server.c
+++ server.c
@@ -6,66 +6,85 @@
 /*   By: fcaquard <fcaquard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/01 15:00:53 by fcaquard          #+#    #+#             */
-/*   Updated: 2021/08/02 18:27:03 by fcaquard         ###   ########.fr       */
+/*   Updated: 2021/08/02 19:56:19 by fcaquard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-int	binary_to_char(char *binary, int bits)
-{
-	int i;
-	int res;
+// int		binary_to_char(char *binary)
+// {
+// 	size_t i;
+// 	size_t res;
+// 	size_t len;
 
-	i = 0;
-	res = 0;
-	while (binary[i])
+// 	len = ft_strlen(binary) - 1;
+// 	i = 0;
+// 	res = 0;
+// 	while (binary[i])
+// 	{
+// 		if (binary[i] == '1')
+// 			res += ft_power(2, len - i);
+// 		i++;
+// 	}
+// 	return (res);
+// }
+
+// int	binary(int digit)
+// {
+// 	static int i = 0;
+// 	static char *binary;
+
+// 	if (i == 0)
+// 	{
+// 		binary = malloc(sizeof(char) * 8 + 1);
+// 		if (!binary)
+// 			return (0);
+// 		binary[8] = '\0';
+// 	}
+
+// 	binary[i] = digit + '0';
+
+// 	if (++i == 8)
+// 	{
+// 		ft_putchar(binary_to_char(binary));
+// 		i = 0;
+// 		free (binary);
+// 		binary = NULL;
+// 	}
+// 	return (0);
+// }
+
+
+void	binary(int digit)
+{
+	static int i = 7;
+	static int c = 0;
+
+	if (digit == 1)
+		c += ft_power(2, i);
+	
+	if (--i < 0)
 	{
-		if (binary[i] == '1')
-			res += ft_power(2, bits - i);
-		i++;
+		ft_putchar(c);
+		i = 7;
+		c = 0;
 	}
-	return (res);
 }
 
-int	binary(int digit)
+void	binary_ones(int num)
 {
-	static int i = 0;
-	static char *binary;
-
-	if (i == 0)
-	{
-		binary = malloc(sizeof(char) * 8 + 1);
-		if (!binary)
-			return (0);
-		binary[8] = '\0';
-	}
-
-	binary[i] = digit + '0';
-
-	if (++i == 8)
-	{
-		ft_putchar(binary_to_char(binary, 7));
-		i = 0;
-		free (binary);
-		binary = NULL;
-	}
-	return (0);
-}
-
-void binary_ones(int num)
-{
-	if (num > 0)
+	if (num)
 		binary(1);
 }
 
-void binary_zeroes(int num)
+void	binary_zeroes(int num)
 {
-	if (num > 0)
+	if (num)
 		binary(0);
 }
 
-int main(void)
+int		main(void)
 {
 	struct sigaction ones = {0};
 	struct sigaction zeroes = {0};
@@ -79,10 +98,9 @@ int main(void)
 
 	ones.sa_handler = binary_ones;
 	zeroes.sa_handler = binary_zeroes;
+	sigaction(SIGUSR1, &ones, NULL);
+	sigaction(SIGUSR2, &zeroes, NULL);
 	
-	while (1){
-		sigaction(SIGUSR1, &ones, NULL);
-		sigaction(SIGUSR2, &zeroes, NULL);
-	}
+	while (1) ;
 	return (0);
 }
