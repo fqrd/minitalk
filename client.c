fdf4ef9e492f61b0c1166eca588fc0296b040dc7
fcaquard <fcaquard@student.s19.be>
2021-08-03 19:38:47 +0200

    working! YAY. Now I should protect everything.

--- client.c
+++ client.c
@@ -6,30 +6,45 @@
 /*   By: fcaquard <fcaquard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/01 17:16:52 by fcaquard          #+#    #+#             */
-/*   Updated: 2021/08/03 14:39:04 by fcaquard         ###   ########.fr       */
+/*   Updated: 2021/08/03 19:32:53 by fcaquard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void	send(int c, int bits, int pid)
+void	send(char *s, int pid)
 {
-	int b;
+	static int	c = 0;
+	static int	bit = -1;
+	static char *str = NULL;
 
-	while (bits >= 0)
+	if (!str)
+		str = s;
+	if (bit < 0)
 	{
-		b = c >> bits;
-		if (b & 1)
-			kill(pid, SIGUSR1);
-		else
-			kill(pid, SIGUSR2);
-		bits--;
-		usleep(0);
+		c = *str++;
+		if (!c)
+			exit(0);
+		bit = 7;
 	}
+
+	if ((c >> bit--) & 1)
+		kill(pid, SIGUSR1);
+	else
+		kill(pid, SIGUSR2);
+}
+
+void	acknowlegded(int sig, siginfo_t *info, void *ucontext)
+{
+	(void) ucontext;
+
+	if (sig == SIGUSR1)
+		send(NULL, info->si_pid);
 }
 
 int		main(int argc, char *argv[])
 {
+	struct sigaction	ack;
 	int		pid;
 	char	*str;
 
@@ -37,8 +52,12 @@ int		main(int argc, char *argv[])
 		return (0);
 	pid = ft_atoi(argv[1]);
 	str = argv[2];
-	while (*str)
-		send(*str++, 7, pid);
-	send(4, 7, pid);
+	ack.sa_flags = SA_SIGINFO;
+	ack.sa_sigaction = acknowlegded;
+	if (sigaction(SIGUSR1, &ack, NULL) == -1)
+		write(1, "error client\n", 13);
+	send (str, pid);
+	while (1)
+		pause();
 	return (0);
 }
--- server.c
+++ server.c
@@ -6,27 +6,28 @@
 /*   By: fcaquard <fcaquard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/01 15:00:53 by fcaquard          #+#    #+#             */
-/*   Updated: 2021/08/03 14:40:43 by fcaquard         ###   ########.fr       */
+/*   Updated: 2021/08/03 19:31:27 by fcaquard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void	handler(int sig)
+void	handler(int sig, siginfo_t *info, void *ucontext)
 {
 	static int i = 7;
 	static int c = 0;
 
+	(void)ucontext;
+	c <<= 1;
 	if (sig == SIGUSR1)
-		c = (c << 1) + 1;
-	else
-		c <<= 1;
+		c++;
 	if (--i < 0)
 	{
 		write(1, &c, 1);
 		i = 7;
 		c = 0;
 	}
+	kill(info->si_pid, SIGUSR1);
 }
 
 int		main(void)
@@ -36,10 +37,11 @@ int		main(void)
 	ft_putnbr(getpid(), 0);
 	ft_putchar('\n');
 	sig.sa_flags = SA_SIGINFO;
-	// sig.sa_sigaction = handler;
-	sig.sa_handler = handler;
-	sigaction(SIGUSR1, &sig, NULL);
-	sigaction(SIGUSR2, &sig, NULL);
+	sig.sa_sigaction = handler;
+	if (sigaction(SIGUSR1, &sig, 0) == -1)
+		write(1, "error\n", 6);
+	if (sigaction(SIGUSR2, &sig, 0) == -1)
+		write(1, "error\n", 6);
 	while (1)
 		pause();
 	return (0);
